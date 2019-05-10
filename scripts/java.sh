cf3e112a42cbc8575b250fad7531a44883a085a6
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-05-10 16:32:26 +0000

    default jdk et jre script

--- scripts/java.sh
+++ scripts/java.sh
@@ -2,10 +2,10 @@
 apt-get install -y default-jdk
 apt-get install -y default-jre
 #install dependcies 
-apt-get install -y software-properties-common dirmngr
-echo  "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8t-java.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
+#apt-get install -y software-properties-common dirmngr
+#echo  "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+#echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8t-java.list
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
 #apt-get install -y oracle-java11-installer
 #echo debconf shared/accepted-oracle-license-v1-1 select true | \
 #  sudo debconf-set-selections
