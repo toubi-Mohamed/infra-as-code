0b4ad0108d4a88d608bf382c40fbec48522b7daa
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-05-10 16:13:07 +0000

    script install docker compose

---
+++ scripts/docker-compose.sh
@@ -0,0 +1,3 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)$
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
