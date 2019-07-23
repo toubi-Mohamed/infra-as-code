03798e814662e11ade56b52c727356396349cd5f
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-07-23 15:54:43 +0100

    Update gitlab.sh

--- scripts/gitlab.sh
+++ scripts/gitlab.sh
@@ -1,5 +1,7 @@
 #install the necessary dependencies
-apt-get install -y curl openssh-server ca-certificates
+apt-get install -y curl \
+openssh-server \
+ca-certificates \
 #install postfix
 echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selections
 echo "postfix postfix/mailname string $hostname.localdomain" | debconf-set-selections
@@ -7,6 +9,9 @@ apt-get install -y postfix
 #install gitlab
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 apt-get install -y gitlab-ce
+#configure  GitLab instance
+gitlab-ctl reconfigure
+gitlab-ctl status 
 
 
 
